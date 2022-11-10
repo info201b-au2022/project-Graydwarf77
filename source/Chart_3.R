3e72c6cd830064fb1943e78f9c1a15be3535d837
Grayson Ellard <graysonellard@gmail.com>
2022-11-10 11:04:32 -0800

    minor edits to chart 3

--- source/Chart_3.R
+++ source/Chart_3.R
@@ -17,10 +17,42 @@ aggregate_hr_by_country <- human_resources %>%
   select(Country, aggregate_human_resources)
 
 combined_mental_health_df <- left_join(adolescent_suicide_rate, aggregate_hr_by_country, by = "Country")
+
+us <- combined_mental_health_df %>%
+  filter(str_detect(Country, "United States"))
+
+rwanda <- combined_mental_health_df %>%
+  filter(str_detect(Country, "Rwanda"))
+
+brazil <- combined_mental_health_df %>%
+  filter(str_detect(Country, "Brazil"))
+
+egypt <- combined_mental_health_df %>%
+  filter(str_detect(Country, "Egypt"))
+
+india <- combined_mental_health_df %>%
+  filter(str_detect(Country, "India"))
+
+malaysia <- combined_mental_health_df %>%
+  filter(str_detect(Country, "Malaysia"))
+
+mexico <- combined_mental_health_df %>%
+  filter(str_detect(Country, "Mexico"))
+
+romania <- combined_mental_health_df %>%
+  filter(str_detect(Country, "Romania"))
+
+japan <- combined_mental_health_df %>%
+  filter(str_detect(Country, "Japan"))
+
+countries_being_observed <- rbind(us, rwanda, brazil, egypt, india, malaysia, mexico, romania, japan)
   
-ggplot(combined_mental_health_df, aes(x = aggregate_human_resources, y = X10to19, label = Country)) + 
+  
+ggplot(countries_being_observed, aes(x = aggregate_human_resources, y = X10to19, label = Country)) + 
   geom_point() +
   geom_text(hjust=.5, vjust=1.5) +
+  xlim(0, 120) +
+  ylim(0, 12) +
   ggtitle("Adolescent Suicide Rates per 100,000 Vs. Total Mental Health Workers per 100,000") +
-  xlab("Total number of Mental Health Workers per 100,000 People") +
+  xlab("Number of Mental Health Workers per 100,000 People") +
   ylab("Suicide Rates for Adolescents Aged 10 to 19 per 100,000 People")
