e6af06699ebacd87e21775a2568d3485ede7f190
Grayson Ellard <graysonellard@gmail.com>
2022-11-09 16:52:52 -0800

    wrangled more data

--- source/Chart_3.R
+++ source/Chart_3.R
@@ -2,6 +2,7 @@ install.packages("ggplot2")
 library("ggplot2")
 library("dplyr")
 library("stringr")
+library("tidyr")
 
 all_suicide_rates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Crude_suicide_rates.csv")
 human_resources <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Human_Resources.csv")
@@ -10,6 +11,10 @@ adolescent_suicide_rate <- all_suicide_rates %>%
   select(Country, Sex, X10to19) %>% 
   filter(str_detect(Sex, "Both sexes"))
 
+aggregate_hr_by_country <- human_resources %>% 
+  replace_na(list(Psychiatrists = 0, Nurses = 0, Social_workers = 0, Psychologists = 0)) %>% 
+  mutate(aggregate_human_resources = Psychiatrists + Nurses + Social_workers + Psychologists) %>% 
+  select(Country, Total_human_resources)
 
-
+combined_mental_health_df <- left_join(adolescent_suicide_rate, aggregate_hr_by_country, by = "Country")
   
