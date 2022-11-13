f8e66152e9b9b5d04a1c71ded2ccbd3b6b05fb4e
Netra Krishnan <114968436+netrak26@users.noreply.github.com>
2022-11-13 13:41:09 -0800

    Update and rename data_access.R to Chart1.R

---
+++ source/Chart1.R
@@ -0,0 +1,20 @@
+# Comparing suicide rates between male and female adolescents
+
+# Load packages
+library(tidyverse)
+library(dplyr)
+library("ggplot2")
+
+# Load data
+suicide_rates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Crude_suicide_rates.csv")
+View(csv)
+
+# Group the sexes and find the mean of their respective suicide rates
+#suiciderates_by_sex <- suicide_rates %>%
+  #group_by(Sex) %>%
+  #summarize(Suicide_rates_adolescents = mean(X10to19))
+
+# Plot the 'suicide_rates' data set, with sex on the x-axis and suicide rates per 100,000 
+# 10 to 19-year-olds on the y-axis
+ggplot(data = suicide_rates) +
+  geom_col(mapping = aes(x = Sex, y = X10to19, color = Sex))
