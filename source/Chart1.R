cb4b6150777203b87b7cb1604743f8918e522925
Netra Krishnan <114968436+netrak26@users.noreply.github.com>
2022-11-14 18:24:35 -0800

    Fixed Chart1

--- source/Chart1.R
+++ source/Chart1.R
@@ -7,14 +7,15 @@ library("ggplot2")
 
 # Load data
 suicide_rates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Crude_suicide_rates.csv")
-View(csv)
 
 # Group the sexes and find the mean of their respective suicide rates
-#suiciderates_by_sex <- suicide_rates %>%
-  #group_by(Sex) %>%
-  #summarize(Suicide_rates_adolescents = mean(X10to19))
+suiciderates_by_sex <- suicide_rates %>%
+  group_by(Sex) %>%
+  summarize(Suicide_rates_adolescents = mean(X10to19))
 
 # Plot the 'suicide_rates' data set, with sex on the x-axis and suicide rates per 100,000 
 # 10 to 19-year-olds on the y-axis
-ggplot(data = suicide_rates) +
-  geom_col(mapping = aes(x = Sex, y = X10to19, fill = Sex))
+ggplot(data = suiciderates_by_sex) +
+  geom_col(mapping = aes(x = Sex, y = Suicide_rates_adolescents, fill = Sex)) +
+  ylab("Suicide Rates in Adolescents") +
+  ggtitle("Comparing Suicide Rates Among Male and Female Adolescents")
