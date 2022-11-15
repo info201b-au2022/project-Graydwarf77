d9b2b3c7b27ad3e2405d68757cfb269b086b58f2
Netra Krishnan <114968436+netrak26@users.noreply.github.com>
2022-11-14 18:52:06 -0800

    Update Chart1.R

--- source/Chart1.R
+++ source/Chart1.R
@@ -15,7 +15,7 @@ suiciderates_by_sex <- suicide_rates %>%
 
 # Plot the 'suicide_rates' data set, with sex on the x-axis and suicide rates per 100,000 
 # 10 to 19-year-olds on the y-axis
-ggplot(data = suiciderates_by_sex) +
+Chart_1 <- ggplot(data = suiciderates_by_sex) +
   geom_col(mapping = aes(x = Sex, y = Suicide_rates_adolescents, fill = Sex)) +
   ylab("Suicide Rates Per 100,000 Adolescents") +
   ggtitle("Comparing Suicide Rates Among Male and Female Adolescents")
