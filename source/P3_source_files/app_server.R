57dbd584c9e5b12b06aba3dd110fb43d87b8ae14
Grayson Ellard <graysonellard@gmail.com>
2022-12-06 00:19:01 -0800

    created widgets for table 3

--- source/P3_source_files/app_server.R
+++ source/P3_source_files/app_server.R
@@ -1,13 +1,20 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidyverse)
 library(shiny)
+library(plotly)
+
+all_suicide_rates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Crude_suicide_rates.csv")
+human_resources <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Human_Resources.csv")
+
+# Pulling adolescent suicide rates from all suicide rates
+adolescent_suicide_rate <- all_suicide_rates %>% 
+  select(Country, Sex, X10to19) %>% 
+  filter(str_detect(Sex, "Both sexes"))
+
+# Combining adolescent suicide rates and HR resources
+combined_mental_health_df <- right_join(adolescent_suicide_rate, human_resources, by = "Country")
+
+mental_health_countries <- combined_mental_health_df %>% 
+  select(Country)
 
 server <- function(input, output) {
     # TBD
--- source/P3_source_files/tabs/tab_panel_chart3.R
+++ source/P3_source_files/tabs/tab_panel_chart3.R
@@ -1,8 +1,38 @@
 # tab_panel_chart3
 
+source("~/info201/project/project-Graydwarf77/source/P3_source_files/app_server.R")
+
 library(shiny)
+
+main_content_chart3 <- mainPanel(
+  plotlyOutput("chart3")
+)
+
+sidebar_content_chart3 <- sidebarPanel(
+  radioButtons(
+    inputId = "chart3_xvar",
+    label = "Please Select Which Type Of Mental Health Worker To View.",
+    choices = list(
+      "Psychiatrists",
+      "Nurses",
+      "Social Workers" = "Social_workers",
+      "Psycologists"
+    ),
+    selected = "Nurses"
+  ),
+  selectInput(
+    inputId = "chart3_country",
+    label = "Please Select Which Countries To View",
+    choices = mental_health_countries,
+    multiple = TRUE
+  )
+)
 
 tab_panel_chart3 <-tabPanel(
     "Chart 3",
-    p("This is chart 3.")
+    p("This is chart 3."),
+    sidebarLayout(
+      sidebar_content_chart3,
+      main_content_chart3
+    )
 )
