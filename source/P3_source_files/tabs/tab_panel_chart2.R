49b8bd81ef552367c433694c5d7bf54687e41d17
keatonstaggs <keatoncole10@gmail.com>
2022-12-06 16:55:25 -0800

    completed chart 2

--- source/P3_source_files/tabs/tab_panel_chart2.R
+++ source/P3_source_files/tabs/tab_panel_chart2.R
@@ -3,6 +3,28 @@
 library(shiny)
 
 tab_panel_chart2 <-tabPanel(
-    "Chart 2",
-    p("This is chart 2.")
+  # Application title
+  titlePanel("Analyzing Suicide Rate for Ages 15-24"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("gender", label = "Select Gender:",
+                   choices = c("male", "female"), 
+                   selected = "male"),
+      selectInput("changeYear", label = "Select a Year:", 
+                  choices = list("1985", "1990", "1995", "2000", "2005", "2010", "2015"), 
+                  selected = "2015"),
+      selectInput("selectCountry", label = "Select a Country: ",
+                  choices = unique_countries
+      )
+    ),
+    
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotlyOutput("plot")
+    )
+  )
 )
+
