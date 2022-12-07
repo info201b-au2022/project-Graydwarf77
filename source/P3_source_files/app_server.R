dc94a32068c5ffa2a210bdafe378c76c30711198
keatonstaggs <keatoncole10@gmail.com>
2022-12-06 17:23:29 -0800

    changed files

--- source/P3_source_files/app_server.R
+++ source/P3_source_files/app_server.R
@@ -4,6 +4,7 @@ library(plotly)
 
 all_suicide_rates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Crude_suicide_rates.csv")
 human_resources <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/Human_Resources.csv")
+chart2_dataset <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-Graydwarf77/main/data/master.csv")
 
 # Pulling adolescent suicide rates from all suicide rates
 adolescent_suicide_rate <- all_suicide_rates %>% 
@@ -16,9 +17,26 @@ combined_mental_health_df <- right_join(adolescent_suicide_rate, human_resources
 mental_health_countries <- combined_mental_health_df %>% 
   select(Country)
 
+# Chart 2 Wrangling
+country_year_filter_plot2 <- function(year_input, country_input, gender_input) {
+  plot2_df <- dataset %>%
+    filter(year == year_input) %>%
+    filter(country == "United States" | country == country_input) %>%
+    filter(sex == gender_input) %>%
+    filter(age == "15-24 years") }
+unique_countries <- dataset %>%
+  distinct(country)
 
 server <- function(input, output) {
-    # TBD
+  output$chart2_plot <- renderPlotly({
+    plot2_title <- paste0("Suicide Rates Comparing the U.S. and ", input$selectCountry, " for (Ages 15-24, per 100,000)")
+    
+    ggplot(data = country_year_filter_plot2(input$changeYear, input$selectCountry, input$gender)) +
+      geom_bar(aes(x = country, y = suicides.100k.pop, fill = country), stat = "identity") +
+      ggtitle(plot2_title) +
+      labs(x = "Countries",
+           y= "Suicide Rate (Per 100,000)")
+  })
 }
 
 
--- source/P3_source_files/tabs/tab_panel_chart2.R
+++ source/P3_source_files/tabs/tab_panel_chart2.R
@@ -2,29 +2,38 @@
 
 library(shiny)
 
+source("~/info201/project/project-Graydwarf77/source/P3_source_files/app_server.R")
+
+
+  chart2_sidebar <- sidebarPanel(
+    radioButtons("gender", label = "Select Gender:",
+                 choices = c("male", "female"), 
+                 selected = "male"),
+    selectInput("changeYear", label = "Select a Year:", 
+                choices = list("1985", "1990", "1995", "2000", "2005", "2010", "2015"), 
+                selected = "2015"),
+    selectInput("selectCountry", label = "Select a Country: ",
+                choices = unique_countries
+    )
+  )
+  
+  
+  # Show a plot of the generated distribution
+ chart2_mainbar <- mainPanel(
+    plotlyOutput("chart2_plot")
+  )
+
+
 tab_panel_chart2 <-tabPanel(
   # Application title
   titlePanel("Analyzing Suicide Rate for Ages 15-24"),
+  sidebarLayout(
+    chart2_sidebar,
+    chart2_mainbar
+    
+  )
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      radioButtons("gender", label = "Select Gender:",
-                   choices = c("male", "female"), 
-                   selected = "male"),
-      selectInput("changeYear", label = "Select a Year:", 
-                  choices = list("1985", "1990", "1995", "2000", "2005", "2010", "2015"), 
-                  selected = "2015"),
-      selectInput("selectCountry", label = "Select a Country: ",
-                  choices = unique_countries
-      )
-    ),
-    
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotlyOutput("plot")
-    )
-  )
+  
 )
 
