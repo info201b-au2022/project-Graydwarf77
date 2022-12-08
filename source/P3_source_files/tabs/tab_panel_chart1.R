0e74d7edb5ee06c486aac10a4551b60b83e0447a
Grayson Ellard <graysonellard@gmail.com>
2022-12-07 18:52:32 -0800

    added caption to chart 1

--- source/P3_source_files/tabs/tab_panel_chart1.R
+++ source/P3_source_files/tabs/tab_panel_chart1.R
@@ -9,5 +9,10 @@ tab_panel_chart1 <-tabPanel(
               label = "Choose a Country",
               "Country names"),
   plotlyOutput("plot_1"),
+  p("This chart shows the comparison between suicide rates among male and female adolescents,
+    with the ability to compare these statistics between countries around the world. By looking 
+    through the suicide rates throughout each country, one can see that a discrepancy between male 
+    and female adolescents' suicide rates is more drastic in some countries than others. Analyzing 
+    this chart can help to understand which groups are allocated fewer mental health resources than others.")
 )
 
